09dca624eb2e9b7b162df1a59c51811b3ebdabb9
William Angarella <William.Angarella1@marist.edu>
2018-09-06 14:48:33 -0400

    Solution to the Curiosity rover excersize.

---
+++ rover.py
@@ -0,0 +1,17 @@
+#CMPT 120L 113
+#Mars Rover Program
+#Author: Will Angarella
+#Created: 9/6/18
+
+def main():
+    
+    time = (34000000 / 186000)
+
+    print("The Mars rover will take ")
+    print(time)
+    print("seconds to communicate photos with Nasa.")
+    
+
+
+
+main()
