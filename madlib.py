af53f12afab4aef20d27b737e799068f77a8c6c2
William Angarella <William.Angarella1@marist.edu>
2018-09-13 14:39:43 -0400

    Upload of madlib.py

---
+++ madlib.py
@@ -0,0 +1,14 @@
+#Madlib Program
+#CMPT120
+#9/13/18
+#Author: Will Angarella
+
+def main():
+    print("Welcome to my madlib! Enter the type of word as prompted for a laugh.")
+    name = input("Please enter a name (doesn't necessarily need to be yours).")
+    verb = input("Please enter a verb ending in 'ing.'")
+    adj = input("Please enter an adjective.")
+
+    print("Well tell me this, " + name + ", are you " + verb + " " + adj + ", punk?")
+
+main()          
