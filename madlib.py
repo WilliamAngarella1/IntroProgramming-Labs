d8f0e43a5cc67eeb7c47cab59eb9b90c2d007c42
William Angarella <William.Angarella1@marist.edu>
2018-09-13 14:50:55 -0400

    Second upload for madlib.py

--- madlib.py
+++ madlib.py
@@ -3,12 +3,21 @@
 #9/13/18
 #Author: Will Angarella
 
-def main():
+def promptForWords():
+    global name, verb, adj
     print("Welcome to my madlib! Enter the type of word as prompted for a laugh.")
     name = input("Please enter a name (doesn't necessarily need to be yours).")
     verb = input("Please enter a verb ending in 'ing.'")
     adj = input("Please enter an adjective.")
 
+
+
+def makeAndPrint():
     print("Well tell me this, " + name + ", are you " + verb + " " + adj + ", punk?")
 
-main()          
+
+def main():
+    promptForWords()
+    makeAndPrint()
+
+main()
