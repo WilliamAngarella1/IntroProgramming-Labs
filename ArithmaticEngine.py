9a00f5387a53d8db1946c4dbe9b5051a6e867bae
William Angarella <William.Angarella1@marist.edu>
2018-10-25 15:14:10 -0400

    First commit of arithmatic engine lab.

---
+++ ArithmaticEngine.py
@@ -0,0 +1,43 @@
+# CMPT 120 - Lab #6
+# Will Angarella
+# 10/25/18
+###
+def getinput():
+    global num1, num2
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+
+def showIntro():
+    print("Welcome to the Arithmetic Engine!")
+    print("=================================\n")
+    print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+def showOutro():
+    print("\nThank you for using the Arithmetic Engine…")
+    print("\nPlease come back again soon!")
+def doLoop():
+    while True:
+        cmd = input("What computation do you want to perform? ")
+        cmd=cmd.lower() #first edit
+        if cmd == "add":
+            getinput()
+            result = num1 + num2
+        elif cmd == "sub":
+            getinput()
+            result = num1 - num2
+        elif cmd == "mult":
+                getinput()
+                result = num1 * num2
+        elif cmd == "div":
+                getinput()
+                result = num1 // num2
+        elif cmd == "quit":
+                break
+        else:
+                print (cmd, "is not a valid command. Enter a valid command.")
+                continue
+        print("The result is " + str(result) + ".\n")
+def main():
+ showIntro()
+ doLoop()
+ showOutro()
+main()
